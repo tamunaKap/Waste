c3381352801d641c17e4742f4131b67cbca33c81
unknown <tamuna.kapanadzee@gmail.com>
2016-02-24 01:38:45 +0400

    text blocks for poyramid done

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,6 +1,16 @@
 ﻿$(function () {
+
+    $(".pyramidText").hide();
     var scene = document.getElementById('scene');
     var parallax = new Parallax(scene, {
         relativeInput: true
+    });
+
+    $(".scene li").click(function () {
+        var target = $(this).data("target");
+        $("#" + target).fadeIn();
+    });
+    $(".pyramidText .glyphicon-remove").click(function () {
+        $(this).parent(".pyramidText").hide();
     });
 })
